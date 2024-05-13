055b0bbbf8660bfc48384539938becfdbd0b1524
Konrad Windszus <konrad.windszus@netcentric.biz>
2024-05-13 13:42:44 +0200

    Enforce replication metadata on segment pages by default
    This closes #15

--- src/main/java/biz/netcentric/filevault/validator/AemReplicationMetadataValidatorFactory.java
+++ src/main/java/biz/netcentric/filevault/validator/AemReplicationMetadataValidatorFactory.java
@@ -43,6 +43,7 @@ public class AemReplicationMetadataValidatorFactory implements ValidatorFactory 
     private static final String OPTION_AGENT_NAMES = "agentNames";
     private static final @NotNull Set<@NotNull String> DEFAULT_AGENT_NAMES = Collections.singleton(ReplicationMetadata.DEFAULT_AGENT_NAME);
     private static final @NotNull Map<Pattern, String> DEFAULT_INCLUDED_NODE_PATH_PATTERNS_AND_TYPES = createDefaultIncludeMap();
+    private static final String RESOURCE_TYPE_SEGMENT_PAGE = "cq/contexthub/components/segment-page";
 
     private static Map<Pattern, String> createDefaultIncludeMap() {
         Map<Pattern, String> map = new HashMap<>();
@@ -52,8 +53,10 @@ public class AemReplicationMetadataValidatorFactory implements ValidatorFactory 
         map.put(Pattern.compile(".*/settings/wcm/templates/[^/]*/policies"), NameConstants.NT_PAGE);
         // mapped content policies (as found by com.day.cq.wcm.core.impl.reference.ContentPolicyReferenceProvider)
         map.put(Pattern.compile(".*/settings/wcm/policies/.*"), RESOURCE_TYPE_CONTENT_POLICY);
-        // and regular context-aware configuration (as found by https://github.com/adobe/aem-core-wcm-components/blob/main/bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/services/CaConfigReferenceProvider.java)
+        // regular context-aware configuration (as found by https://github.com/adobe/aem-core-wcm-components/blob/main/bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/services/CaConfigReferenceProvider.java)
         map.put(Pattern.compile("/(apps|conf)/.*/(sling:configs|settings/cloudconfigs)/.*"), NameConstants.NT_PAGE);
+        // segment pages (as found by com.day.cq.personalization.impl.TargetedComponentReferenceProvider)
+        map.put(Pattern.compile("/(apps|conf)/.*/jcr:content"), RESOURCE_TYPE_SEGMENT_PAGE);
         return Collections.unmodifiableMap(map);
     }
 
